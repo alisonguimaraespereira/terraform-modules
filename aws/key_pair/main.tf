4ffc6c434f77fa1bef60b02c93f9dfe7d5185914
Alison Guimaraes <guimaraes.asp@gmail.com>
2021-07-08 11:28:40 -0300

    KeyPair

--- aws/key_pair/main.tf
+++ aws/key_pair/main.tf
@@ -1,4 +1,8 @@
 resource "aws_key_pair" "default" {
   key_name   = "${var.aws_key_pair_name}"
   public_key = "${var.key_pub}"
+
+  provisioner "local-exec" { 
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./Key.pem"
+  }
 }
