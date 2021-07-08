7ab9f9e19acf10b5996bca203e92c7c889763a3d
Alison Guimaraes <guimaraes.asp@gmail.com>
2021-07-08 01:46:46 -0300

    Alterando output do modulo nlb

--- aws/nlb/aws-lb/outputs.tf
+++ aws/nlb/aws-lb/outputs.tf
@@ -1,7 +1,7 @@
 output "nlb_arn" {
-  value = var.internal ? concat(aws_lb.nlb_internal.*.arn, list(""))[0] : concat(aws_lb.nlb_external.*.arn, list(""))[0]
+  value = var.internal ? concat(aws_lb.nlb_internal.*.arn, tolist([""]))[0] : concat(aws_lb.nlb_external.*.arn, tolist([""]))[0]
 }
 
 output "dns_name" {
-  value = var.internal ? concat(aws_lb.nlb_internal.*.dns_name, list(""))[0] : concat(aws_lb.nlb_external.*.dns_name, list(""))[0]
+  value = var.internal ? concat(aws_lb.nlb_internal.*.dns_name, tolist([""]))[0] : concat(aws_lb.nlb_external.*.dns_name, tolist([""]))[0]
 }
