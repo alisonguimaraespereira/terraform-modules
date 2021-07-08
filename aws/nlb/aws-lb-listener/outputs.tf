a18347d55db233f48f2afd6d58a4089c78098257
Alison Guimaraes <guimaraes.asp@gmail.com>
2021-07-08 01:41:10 -0300

    Alterando output do modulo nlb

--- aws/nlb/aws-lb-listener/outputs.tf
+++ aws/nlb/aws-lb-listener/outputs.tf
@@ -1,7 +1,7 @@
 output "arn_https" {
-  value = element(concat(aws_lb_listener.TLS.*.arn, list("")), 0)
+  value = element(concat(aws_lb_listener.TLS.*.arn, tolist([""])), 0)
 }
 
 output "arn_http" {
-  value = element(concat(aws_lb_listener.TCP.*.arn, list("")), 0)
+  value = element(concat(aws_lb_listener.TCP.*.arn, tolist([""])), 0)
 }
